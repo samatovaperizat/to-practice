bda48c1136e2ff6d62949f67832163e4638a48e4
Perizat <samatovaperizat@gmail.com>
2022-06-15 22:10:45 +0200

    practicing the JavaScript

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let country = prompt("What country are you from?");
+
+if (country === "Portugal", "Bishkek") {
+  alert("You are cool");
+}
+
+if (country !== "Portugal") {
+  alert("Too bad for you");
+}
+
+let person = prompt("Do you wear the glass?");
+
+if (person === "yes") {
+    alert(" I hope your eyes gets better soon");
+}
+
+let theCar = prompt("which car do you like?");
+
+if (theCar === "Fiat 500", "Audi"){
+    alert("you have a nice taste");
+}
+
+if (theCar !== "Fiat 500", "Audi"){
+    alert(" You don't have a taste");
+}
+
+let age = prompt("How old are you?");
+
+if (age < 18) {
+  alert("You cannot apply");
+} else {
+  alert("You can apply");
+}
+
+let ice = prompt("is it too cold?");
+if (ice = cold) {
+    alert
+
+}
